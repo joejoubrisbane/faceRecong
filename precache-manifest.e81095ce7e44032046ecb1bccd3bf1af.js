2da8e6752359ca5c48ae8f2c6356ee4d671d9387
chi-liang ju <pk0204dg888@gmail.com>
2019-02-14 16:21:25 +1000

    Updates

---
+++ precache-manifest.e81095ce7e44032046ecb1bccd3bf1af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61d3a24ae6e1793ef67e",
+    "url": "/faceRecong/static/js/runtime~main.61d3a24a.js"
+  },
+  {
+    "revision": "98222daa1e6bc861c95f",
+    "url": "/faceRecong/static/js/main.98222daa.chunk.js"
+  },
+  {
+    "revision": "bf9ee4b7fbfea78e834a",
+    "url": "/faceRecong/static/js/2.bf9ee4b7.chunk.js"
+  },
+  {
+    "revision": "98222daa1e6bc861c95f",
+    "url": "/faceRecong/static/css/main.dd823457.chunk.css"
+  },
+  {
+    "revision": "bf9ee4b7fbfea78e834a",
+    "url": "/faceRecong/static/css/2.c82bf1f1.chunk.css"
+  },
+  {
+    "revision": "5de11366fe8a94c2640370743464777f",
+    "url": "/faceRecong/index.html"
+  }
+];
