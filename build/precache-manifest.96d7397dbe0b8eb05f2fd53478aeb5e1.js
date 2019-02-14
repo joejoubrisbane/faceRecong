6837142a39d299aa2884fa1b541a59454354eeaa
chi-liang ju <pk0204dg888@gmail.com>
2019-02-14 15:48:27 +1000

    add build

---
+++ build/precache-manifest.96d7397dbe0b8eb05f2fd53478aeb5e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6dbc5fb70505309e3487",
+    "url": "/static/js/main.6dbc5fb7.chunk.js"
+  },
+  {
+    "revision": "bf9ee4b7fbfea78e834a",
+    "url": "/static/js/2.bf9ee4b7.chunk.js"
+  },
+  {
+    "revision": "6dbc5fb70505309e3487",
+    "url": "/static/css/main.dd823457.chunk.css"
+  },
+  {
+    "revision": "bf9ee4b7fbfea78e834a",
+    "url": "/static/css/2.c82bf1f1.chunk.css"
+  },
+  {
+    "revision": "13aa2079cb4c04ea1cce5057a60dd9a9",
+    "url": "/index.html"
+  }
+];
