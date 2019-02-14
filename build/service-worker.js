2b58035e1bf5da86048ad2501c8ba32475c60bf5
chi-liang ju <pk0204dg888@gmail.com>
2019-02-14 17:07:20 +1000

    part one don

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96d7397dbe0b8eb05f2fd53478aeb5e1.js"
+  "/faceRecong/precache-manifest.e81095ce7e44032046ecb1bccd3bf1af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/faceRecong/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
